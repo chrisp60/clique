932d823d4c6867c13ddcd52f0c8a677891453fa4
chrisp60 <89366859+chrisp60@users.noreply.github.com>
2024-02-14 14:33:58 -0500

    feat(client): websocket into signal

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+//! Wasm frontend using Leptos
+//!
+//! ```not_rust
+//! trunk build --release
+//! ```
+
+use gloo_net::websocket::futures::WebSocket;
+use leptos::*;
+use web_sys::WebSocket;
+
+const WS_ADDR: &str = concat!("ws://", env!("ADDR"), "ws");
+
+/// Just tricking rustc a bit.
+#[allow(non_snake_case)]
+fn Ok<T>(t: T) -> Result<T> {
+    std::result::Result::Ok(t)
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error(transparent)]
+struct Error(#[from] eyre::Report);
+
+impl IntoView for Error {
+    fn into_view(self) -> View {
+        view! {
+            <p>
+                <pre>{self.to_string()}</pre>
+            </p>
+        }
+        .into_view()
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    leptos::mount_to_body(App);
+    Ok(())
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (read, write) = create_signal(WebSocket::open(WS_ADDR).unwrap());
 }
